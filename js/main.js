8f9565fd25a0ec8efb5e3cc559b808958b8a7dd6
Martín González <m@martingonzalez.net>
2016-03-31 13:09:34 +0200

    implement lazyload

--- js/main.js
+++ js/main.js
@@ -19,3 +19,9 @@ map.addLayer(googleLayer);*/
 
 // balance text
 $('.introHeadline').balanceText();
+
+$(".lazy").unveil(200, function() {
+  $(this).load(function() {
+    this.style.opacity = 1;
+  });
+});
