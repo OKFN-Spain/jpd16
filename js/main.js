82ecded875273e93c086f6392b76e90ce9fa8a9b
Martín González <m@martingonzalez.net>
2016-03-31 13:25:00 +0200

    share links

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-/*
-* Leaflet map
-*/
-/*var map = L.map('map').setView([40.41644,-3.69359], 15);
-
-// add a marker in the given location, attach some popup content to it and open the popup
-// note that the street numbering is a bit odd: the actual entry is at the round part of the building (NE corner)
-L.marker([40.41044, -3.69359]).addTo(map)
-    .bindPopup('Medialab-Prado')
-    .openPopup();
-
-//	L.marker([40.42043,-3.70222]).addTo(map)
-//	.bindPopup('Fundación Telefonica')
-map.scrollWheelZoom.disable();
-
-// add Google layer
-var googleLayer = new L.Google('ROADMAP');
-map.addLayer(googleLayer);*/
-
 // balance text
 $('.introHeadline').balanceText();
 
